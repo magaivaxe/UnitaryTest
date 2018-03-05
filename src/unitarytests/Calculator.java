9b0063713a85f502988c1dea7cd455149e9250bf
magaivaxe <mpg.salgad@gmail.com>
2018-03-05 14:14:05 -0500

    Work

---
+++ src/unitarytests/Calculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitarytests;
+
+/**
+ * The interfaces create un type comme mouseListenner or others abstracts
+ * classes. After implements the methods will be add automaticaly
+ * @author eleves
+ */
+public interface Calculator
+{
+    int multiply(int a, int b);
+    int divide(int a, int b);
+    int add(int a, int b);
+    int substract(int a, int b);
+}
---
+++ src/unitarytests/UnitaryTests.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitarytests;
+
+/**
+ *
+ * @author eleves
+ */
+public class UnitaryTests implements Calculator
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+
+    @Override
+    public int multiply(int a, int b)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int divide(int a, int b)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int add(int a, int b)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int substract(int a, int b)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
